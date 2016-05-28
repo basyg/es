636c0c302c9f35c49cf7754fcfe9a4a7195b639c
Basygysov Denis <basygysov@gmail.com>
2016-05-28 13:57:54 +0300

    Последние версии сгенерированных кодов

--- src/es/EntityComponents.hx
+++ src/es/EntityComponents.hx
@@ -1,12 +1,14 @@
 package es;
 class EntityComponents
 {
-	@:noCompletion public var __app_components_Speed:app.components.Speed = null;
-	@:noCompletion public var __app_components_Position:app.components.Position = null;
+#if !macro
 	@:noCompletion public var __app_components_Angle:app.components.Angle = null;
+	@:noCompletion public var __app_components_Position:app.components.Position = null;
+	@:noCompletion public var __app_components_Speed:app.components.Speed = null;
 
-	@:noCompletion public var __app_components_Position__app_components_Speed__listNo:Int = -1;
 	@:noCompletion public var __app_components_Angle__app_components_Position__app_components_Speed__listNo:Int = -1;
-	@:noCompletion public var __app_components_Speed__listNo:Int = -1;
+	@:noCompletion public var __app_components_Position__app_components_Speed__listNo:Int = -1;
 	@:noCompletion public var __app_components_Position__listNo:Int = -1;
+	@:noCompletion public var __app_components_Speed__listNo:Int = -1;
+#end
 }
--- src/es/EntitySystemLists.hx
+++ src/es/EntitySystemLists.hx
@@ -1,8 +1,10 @@
 package es;
 class EntitySystemLists
 {
-	@:noCompletion public var __app_components_Position__app_components_Speed__list:ds.Arr<Entity> = new ds.Arr();
+#if !macro
 	@:noCompletion public var __app_components_Angle__app_components_Position__app_components_Speed__list:ds.Arr<Entity> = new ds.Arr();
-	@:noCompletion public var __app_components_Speed__list:ds.Arr<Entity> = new ds.Arr();
+	@:noCompletion public var __app_components_Position__app_components_Speed__list:ds.Arr<Entity> = new ds.Arr();
 	@:noCompletion public var __app_components_Position__list:ds.Arr<Entity> = new ds.Arr();
+	@:noCompletion public var __app_components_Speed__list:ds.Arr<Entity> = new ds.Arr();
+#end
 }
